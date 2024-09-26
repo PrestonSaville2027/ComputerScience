581339627bc5b56433ee53a60768097992252c79
PrestonSaville2027 <824408@mystma.org>
2024-09-26 15:11:31 -0500

    Mad Lib: the better version

--- Assignments/SimpleMadLib.py
+++ Assignments/SimpleMadLib.py
@@ -1,6 +1,7 @@
 # Once upon a time there was a [strange animal] in the time period of [random time period].
 # Everyday this animal would [activity] and [another acivity], 
 # but one day this animal saw [an object] and it changed the animal's life. 
+# The animal touched the object and [action] and looked at the object from [number] feet away.
 print("This is a Mad Lib, you follow the instructions and then when finished read the very silly story!")
 print("name a strange animal")
 animal = user_input =input("Enter something")
@@ -12,5 +13,9 @@ print("name another activity")
 activity2 = user_input =input("Enter something")
 print("name a object")
 object = user_input =input("Enter something")
-print("Once upon a time there was a " + animal + " in the time period of " + period + ". Everyday this animal would " + activity + " and " + activity2 + ", but one day this animal saw " + object + " and it changed the animal's life." )
-print("Very silly story, right? Rememeber you can always go back!")
+print("name a action")
+number = user_input =input("Enter something")
+print("type a number")
+action = user_input =input("Enter something")
+print("Once upon a time there was a " + animal + " in the time period of " + period + ". Everyday this animal would " + activity + " and " + activity2 + ", but one day this animal saw " + object + " and it changed the animal's life. The animal touched the object and " + action + " and looked at the object from " + number + " feet away.")
+print("Very silly story, right?")
