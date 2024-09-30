70a01415291a06291f6625171b1ec1eac5da5831
PrestonSaville2027 <824408@mystma.org>
2024-09-30 14:57:32 -0500

    Top Five Movies Assignment

---
+++ Assignments/Functions.py
@@ -0,0 +1,24 @@
+def my_functions():
+ print("1. Lord of the Rings")
+print("2. Pacific Rim")
+print("3. Rons Gone Wrong")
+print("4. Get Smart")
+print("5. Star Wars")
+
+def add(x, y):
+   print(x + y)
+
+add(5, 10)
+add(2, 2)
+add(15, 15)
+add("cat", "dog")
+
+def movie(z):
+  print(z)
+
+movie("1. Lord of the Ring")
+movie("2. Pacific Rim")
+movie("3. Rons Gone Wrong")
+movie("4. Get Smart")
+movie("5. Star Wars")
+
---
+++ Assignments/Top Five Movies.py
@@ -0,0 +1,15 @@
+def movie(movie1, movie2, movie3, movie4, movie5):
+    print("1. " + movie1)
+    print("2. " + movie2)
+    print("3. " + movie3)
+    print("4. " + movie4)
+    print("5. " + movie5)
+    
+top_movies = movie ("Pacific Rim", "Star Wars", "Lord of the Rings", "Get Smart", "Hunger Games")
+
+    
+
+ 
+ 
+ 
+ 
