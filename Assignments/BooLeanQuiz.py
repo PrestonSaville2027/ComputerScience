e2616200f61ba7e525300dbb1005084ff0bd6721
PrestonSaville2027 <824408@mystma.org>
2024-10-04 14:57:29 -0500

    BooLeanQuiz

---
+++ Assignments/BooLeanQuiz.py
@@ -0,0 +1,16 @@
+print("This is a quiz, please answer the questions as best as you can")
+
+answer = int(input("Give a whole number that is less than 5.\n"))
+print(answer < 5)
+
+answer2 = int(input("Give a whole number that is more than 9.\n"))
+print(answer2 > 9)
+
+answer3 = int(input("Give a whole number that is equal to 6.\n"))
+print(answer3 == 6 )
+
+answer4 = int(input("Give a whole number that is less than or equal to 5.\n"))
+print(answer4 <= 5)
+
+answer5 = int(input("Give a whole number that is less than 1.\n"))
+print(answer5 < 1)
